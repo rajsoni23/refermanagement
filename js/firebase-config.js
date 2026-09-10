ed9cc8606ab17a5caf1fe76ecf1f552e29eca919
Raj Soni <137527742+rajsoni23@users.noreply.github.com>
2026-09-10 08:30:12 +0530

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,1 @@
+export const firebaseConfig={apiKey:"AIzaSyCIgAGDMgy01emkzFXX6J4tK-bj7kH21QU",authDomain:"rbskrefer.firebaseapp.com",projectId:"rbskrefer",storageBucket:"rbskrefer.firebasestorage.app",messagingSenderId:"739233118807",appId:"1:739233118807:web:0d0f79bdc82ceeba1a4112"};
